0a2167a2b987a027902e9eb95f2d76e2138604d9
Semyonenko Ivan <vsem2004@gmail.com>
2023-10-18 20:35:24 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sass = require('gulp-sass')(require('sass'))
+var server = require("browser-sync").create()
+var csso = require("gulp-csso")
+var autoprefixer = require("autoprefixer")
+var sourcemap = require("gulp-sourcemaps")
+var rename = require("gulp-rename")
+var postcss = require("gulp-postcss");
+var imagemin = require("gulp-imagemin")
+var del = require("del");
+
+gulp.task("css", function () {
+    return gulp.src("src/assets/scss/style.scss")
+        .pipe(plumber())
+        .pipe(sourcemap.init())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest("public/assets/css"))
+        .pipe(csso())
+        .pipe(rename("style.min.css"))
+        .pipe(sourcemap.write("."))
+        .pipe(gulp.dest("public/assets/css"))
+        .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+    return gulp.src("src/assets/image/**/*.{png,jpg,svg}")
+        .pipe(imagemin([
+            imagemin.optipng({
+                optimizationLevel: 3
+            }),
+            imagemin.mozjpeg({
+                progressive: true
+            }),
+            imagemin.svgo()
+        ]))
+
+        .pipe(gulp.dest("public/image"));
+});
+
+gulp.task("html", function() {
+    return gulp.src("src/view/*.html")
+      .pipe(gulp.dest("public"));
+  });
+
+gulp.task("copy", function () {
+    return gulp.src([
+            "src/assets/fonts/**/*.{woff,woff2}",
+            "src/assets/image/**",
+            "src/assets/js/**",
+            "src/*.ico"
+        ], {
+            base: "src"
+        })
+        .pipe(gulp.dest("public"));
+});
+
+gulp.task("clean", function () {
+    return del("public");
+});
+
+gulp.task("build", gulp.series(
+    "clean",
+    "images",
+    "css",
+    "html"
+));
+
+gulp.task("server", function () {
+    server.init({
+        server: "public/",
+    });
+
+    gulp.watch("src/assets/scss/**/*.scss", gulp.series("css"));
+    gulp.watch("src/view/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+    server.reload();
+    done();
+});
+
+gulp.task("start", gulp.series("build", "server"));
