696715beb30e480d85971051b745c042466873f5
Semyonenko Ivan <vsem2004@gmail.com>
2023-12-07 11:55:20 +0300

    done news page and about page adaptive

--- src/assets/js/swiper-toggle.js
+++ src/assets/js/swiper-toggle.js
@@ -2,7 +2,6 @@ const canBeViewedSwippers = document.querySelectorAll(".can-be-viewed");
 
 canBeViewedSwippers.forEach((swipper) => {
     swipper.addEventListener(("click"), (e) => {
-        console.log("AWD");
         e.currentTarget.classList.toggle("slider--viewed");
     })
 });
